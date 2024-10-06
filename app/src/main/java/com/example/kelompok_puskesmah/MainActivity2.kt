219b9957b030a2f60e4e48d0c3b1682b8722bee8
M.Rafli Albukhori <rr5428820@gmail.com>
2024-10-06 12:20:31 +0700

    AddHomeActivity

--- app/src/main/java/com/example/kelompok_puskesmah/MainActivity2.kt
+++ app/src/main/java/com/example/kelompok_puskesmah/MainActivity2.kt
@@ -1,13 +1,22 @@
 package com.example.kelompok_puskesmah
 
-
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.kelompok_puskesmah.R
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2) // Menghubungkan layout activity_main2.xml
+
+        // Cari tombol daftar dengan ID
+        val daftarButton = findViewById<Button>(R.id.signup)
+
+        // Ketika tombol daftar di klik, pindah ke HomeActivity
+        daftarButton.setOnClickListener {
+            val intent = Intent(this@MainActivity2, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
