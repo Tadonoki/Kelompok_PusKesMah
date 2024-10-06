8fac64691f098776e966e637ffb0a236193fbfa7
Tadonoki <khailanilupi2005@gmail.com>
2024-10-06 11:40:01 +0700

    Tadonoki

---
+++ app/src/main/java/com/example/kelompok_puskesmah/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.kelompok_puskesmah
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kelompok_puskesmah.MainActivity2
+import com.example.kelompok_puskesmah.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // Menghubungkan dengan layout activity_main.xml
+
+        // Cari tombol daftar dengan ID
+        val daftarButton = findViewById<Button>(R.id.daftar)
+
+        // Ketika tombol daftar di klik, pindah ke MainActivity2
+        daftarButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kelompok_puskesmah/MainActivity2.kt
@@ -0,0 +1,13 @@
+package com.example.kelompok_puskesmah
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kelompok_puskesmah.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2) // Menghubungkan layout activity_main2.xml
+    }
+}
