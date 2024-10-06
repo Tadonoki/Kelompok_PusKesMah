e362e003ea0c8632a70a4b22d6557a1cc77df3e5
M.Rafli Albukhori <rr5428820@gmail.com>
2024-10-06 14:15:22 +0700

    AddHome and Search Bar

--- app/src/main/java/com/example/kelompok_puskesmah/SignInActivity.kt
+++ app/src/main/java/com/example/kelompok_puskesmah/SignInActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kelompok_puskesmah
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin) // Menghubungkan layout activity_home.xml
+        setContentView(R.layout.activity_signin) // Menghubungkan layout activity_signin.xml
+
+        // Cari tombol masuk dengan ID
+        val masukButton = findViewById<Button>(R.id.masuk) // Pastikan Anda memiliki ID yang benar
+
+        // Ketika tombol masuk di klik, pindah ke HomeActivity
+        masukButton.setOnClickListener {
+            val intent = Intent(this@SignInActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
