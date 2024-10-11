b72fea8f4841956d9cab54ed5c711498a8535cfc
Tadonoki <khailanilupi2005@gmail.com>
2024-10-12 00:07:31 +0700

    Chat Dokter dan Integrasi

--- app/src/main/java/com/example/kelompok_puskesmah/Detaildokter.kt
+++ app/src/main/java/com/example/kelompok_puskesmah/Detaildokter.kt
@@ -17,10 +17,17 @@ class Detaildokter : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_detaildokter)
         val buttonKonsul1 = findViewById<ImageButton>(R.id.backtokonsultasi)
+        val buttonDokter1 = findViewById<Button>(R.id.chatdokter1)
 
         // Ketika tombol konsultasi kembali ke home di klik, pindah ke HomeActivity
         buttonKonsul1.setOnClickListener {
             val intent = Intent(this@Detaildokter, konsultasi::class.java)
+            startActivity(intent)
+        }
+
+        // Ketika tombol detail dokter 1 di klik, pindah ke Detaildokter
+        buttonDokter1.setOnClickListener {
+            val intent = Intent(this@Detaildokter, chatdokterkonsultasi::class.java)
             startActivity(intent)
         }
 
