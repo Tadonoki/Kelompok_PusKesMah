1de242ba1748f1ef4a369c158b3fe44d8fc7768e
Tadonoki <khailanilupi2005@gmail.com>
2024-10-12 00:07:41 +0700

    Chat Dokter dan Integrasi

---
+++ app/src/main/java/com/example/kelompok_puskesmah/chatdokterkonsultasi.kt
@@ -0,0 +1,25 @@
+package com.example.kelompok_puskesmah
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageButton
+
+class chatdokterkonsultasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_chatdokterkonsultasi)
+        val buttonKonsul = findViewById<ImageButton>(R.id.backtodetaildokter)
+
+        buttonKonsul.setOnClickListener {
+            val intent = Intent(this@chatdokterkonsultasi, Detaildokter::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
