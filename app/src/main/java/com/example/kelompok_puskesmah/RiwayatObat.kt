f462bc7f71a2c40318425732ac77acbef97ae87e
Tadonoki <khailanilupi2005@gmail.com>
2024-10-13 22:30:04 +0700

    fix: all

--- app/src/main/java/com/example/kelompok_puskesmah/RiwayatObat.kt
+++ app/src/main/java/com/example/kelompok_puskesmah/RiwayatObat.kt
@@ -3,6 +3,7 @@ package com.example.kelompok_puskesmah
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,56 +14,45 @@ class RiwayatObat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge() // Pastikan ini didefinisikan jika kamu menggunakannya
-        setContentView(R.layout.activity_riwayat) // Pastikan layout ini ada
+        setContentView(R.layout.activity_riwayat_obat) // Pastikan layout ini ada
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val beranda = findViewById<ImageButton>(R.id.beranda)
+        val riwayat = findViewById<ImageButton>(R.id.riwayat)
+        val pesan = findViewById<ImageButton>(R.id.pesan)
+        val kehome = findViewById<ImageButton>(R.id.kehome)
+        val janjioffline = findViewById<Button>(R.id.riwayatoffline)
+
+        janjioffline.setOnClickListener {
+            val intent = Intent(this@RiwayatObat, Riwayat::class.java)
+            startActivity(intent)
         }
-        val ButtonHome = findViewById<ImageButton>(R.id.beranda)
 
-
-//        val ButtonPesan = findViewById<ImageButton>(R.id.pesan)
-
-        // Ketika tombol masuk di klik, pindah ke Beranda
-        ButtonHome.setOnClickListener {
+        kehome.setOnClickListener {
             val intent = Intent(this@RiwayatObat, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        beranda.setOnClickListener {
+            val intent = Intent(this@RiwayatObat, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        riwayat.setOnClickListener {
+            val intent = Intent(this@RiwayatObat, riwayat::class.java)
+            startActivity(intent)
+        }
+
+        pesan.setOnClickListener {
+            val intent = Intent(this@RiwayatObat, Pesan::class.java)
             startActivity(intent)
         }
 
         val ButtonAkun = findViewById<ImageButton>(R.id.akun)
 
-        // Ketika tombol masuk di klik, pindah ke Riwayat
-        val riwayatButton = findViewById<ImageButton>(R.id.riwayat)
-        riwayatButton.setOnClickListener {
-            val intent = Intent(this@RiwayatObat, Riwayat::class.java)
-            startActivity(intent)
-        }
-
-
-//        // Ketika tombol masuk di klik, pindah ke Akun
-//        ButtonPesan.setOnClickListener {
-//            val intent = Intent(this@Riwayat, Pesan::class.java)
-//            startActivity(intent)
-//        }
-
-
-
         // Ketika tombol masuk di klik, pindah ke Akun
         ButtonAkun.setOnClickListener {
             val intent = Intent(this@RiwayatObat, akun::class.java)
             startActivity(intent)
-        }
-
-
-    }
-
-
-
-    companion object {
-        fun setOnClickListener(view: View, function: () -> Unit) {
-            view.setOnClickListener { function() }
         }
     }
 }
